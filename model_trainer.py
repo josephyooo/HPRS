329711f0d051f78d419c0994d9058c788157c243
Joseph Yoo <fizzyinc3@gmail.com>
2024-04-10 01:52:14 -0400

    Segment into modules

---
+++ model_trainer.py
@@ -0,0 +1,81 @@
+import time
+import os
+from tempfile import TemporaryDirectory
+
+import torch
+
+# Reference: https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+# Generic method to train a model, refer to googlenet_scalp_training.ipynb for an example
+def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs, device, augmenter=None):
+    since = time.time()
+
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+        dataset_sizes = {phase: len(dataloader.dataset) for phase, dataloader in dataloaders.items()}
+
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch+1}/{num_epochs} | Learning Rate {optimizer.param_groups[0]["lr"]}')
+            print('-' * 10)
+
+            # Each epoch has a training and validation phase
+            for phase in dataloaders.keys():
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()   # Set model to evaluate mode
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    if augmenter:
+                        inputs = augmenter(inputs)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad(set_to_none=True)
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+        # load best model weights
+        model.load_state_dict(torch.load(best_model_params_path))
+    return model
